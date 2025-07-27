2265efaea3551f34749734bcd255dd66fe4801e9
Vishnu Kalanad <dxbvishnu@gmail.com>
2025-07-27 23:20:11 +0530

    Initial commit

---
+++ project-management/src/components/sidebar.jsx
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+    return <aside>
+        <h2>Projects</h2>
+        <div>
+            <button>+New Project</button>
+        </div>
+        <ul></ul>
+    </aside>
+}
